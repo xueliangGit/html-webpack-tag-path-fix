2022fa35b7359348a5e1ff64eb85b4a82d0a3dea
xuxueliang <xu.xueliang@163.com>
2020-03-20 16:49:53 +0800

    ed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: xuxueliang
  * @Date: 2019-12-06 11:25:39
- * @LastEditors  : xuxueliang
- * @LastEditTime : 2019-12-21 13:15:31
+ * @LastEditors: xuxueliang
+ * @LastEditTime: 2020-03-20 16:48:54
  */
 let HtmlWebpackPlugin = require('html-webpack-plugin')
 function getPath (outputName) {
@@ -34,7 +34,7 @@ function mainDo (data, cb) {
   cb(null, data)
 }
 class HtmlWebpackTagPathFix {
-  constructor (options) {
+  constructor(options) {
     this.options = Object.assign(
       {
         injectCode: ''
@@ -47,27 +47,36 @@ class HtmlWebpackTagPathFix {
     compiler.hooks.compilation.tap('HtmlWebpackTagPathFix', compilation => {
       // console.log('The compiler is starting a new compilation...')
       // Static Plugin interface |compilation |HOOK NAME | register listener
-      // 添加骨架屏
-      compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
-        'HtmlWebpackTagPathFix',
-        (data, cb) => {
-          // console.log('this.options.injectCode', this.options.injectCode)
-          if (this.options.injectCode) {
-            data.html = data.html.replace(
-              '<div id=app></div>',
-              `<div id=app>${this.options.injectCode}</div>`
-            )
-          }
-          cb(null, data)
-        }
-      )
       // 处理多层级的html静态资源路径问题
       if (compiler.hooks) {
-        compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync(
-          // alter - asset - tags
-          'HtmlWebpackTagPathFix', // <-- Set a meaningful name here for stacktraces
-          mainDo
-        )
+        // 添加骨架屏
+        if (this.options.injectCode) {
+          compilation.hooks.htmlWebpackPluginAfterHtmlProcessing && compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
+            'HtmlWebpackTagPathFix',
+            (data, cb) => {
+              // 支持用方法去处理
+              if (typeof this.options.injectCode === 'function') {
+                let nhtml = this.options.injectCode(data.html)
+                if (nhtml) {
+                  data.html = nhtml
+                }
+              } else {
+                data.html = data.html.replace(
+                  new RegExp(`<div id=app>[ ]*</div>`),
+                  `<div id=app>${ this.options.injectCode }</div>`
+                )
+                cb(null, data)
+              }
+            }
+          )
+        }
+        if (compilation.hooks.htmlWebpackPluginAlterAssetTags) {
+          compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync(
+            // alter - asset - tags
+            'HtmlWebpackTagPathFix', // <-- Set a meaningful name here for stacktraces
+            mainDo
+          )
+        }
       } else {
         //
         HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync(
